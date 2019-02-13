15dd31bda890568548c3953e0663efd6c9d769d7
Galih Laras Prakoso <galihlprakoso@gmail.com>
2019-02-13 14:22:46 +0700

    Create RemoteConfigPackage.java

---
+++ RemoteConfigPackage.java
@@ -0,0 +1,30 @@
+package com.sayurbox.bridge.remoteconfig;
+
+/*
+    @author Galih Laras Prakoso | Github : galihlprakoso
+*/
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RemoteConfigPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new RemoteConfigModule(reactContext));
+
+        return modules;
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
