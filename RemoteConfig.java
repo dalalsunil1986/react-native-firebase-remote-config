845d400602c5fec96390cd54b80a7bf749e52c28
Galih Laras Prakoso <galihlprakoso@gmail.com>
2019-02-18 14:00:41 +0700

    Update RemoteConfig.java

--- RemoteConfig.java
+++ RemoteConfig.java
@@ -14,7 +14,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
-class RemoteConfig implements RemoteConfig{
+class RemoteConfig implements RemoteConfigInterface{
 
     private static final String NAME = "RemoteConfig";
 
